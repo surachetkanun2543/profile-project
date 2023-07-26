affe51c65b377a3e52c8be0a87927241717c4bf8
Booksurachet <surache2543@hotmail.co.th>
2023-07-26 23:58:55 +0700

    add home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -17,7 +17,7 @@
         </div>
         <div class="hidden lg:flex lg:flex-1 lg:justify-end">
         </div>
-      </nav>
+      </nav> 
     </header>
 
     <div class="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80" aria-hidden="true">
